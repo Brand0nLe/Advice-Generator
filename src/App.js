27447e2445e03bb2e706b21038d50f68a524baeb
Brand0nLe <93brandonle@gmail.com>
2023-03-14 21:06:17 -0700

    update and adding pedro's peer review in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 //Brandon Le
-//3-14-23
+//3-14-23 @ 9PM
 //Advice Generator in React
 //With the provided assets I rebuilt a react app that uses bootstrap, css, and html. We used ASYNC to access the advice API and this will be hosted on azure.
-//Peer Review By Pedro 
+//Peer Review By Pedro Castaneda - Excellent work getting the divider svg to alternate between mobile and desktop. Also good job on chaging the favicon. One thing I noticed you missed were the quotes, but  overall great work!
 
 import AdviceGenerator from './advicegeneratorcomponent/AdviceGenerator';
 import './advicegeneratorcomponent/AdviceGenerator.css';
